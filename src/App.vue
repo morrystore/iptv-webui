d915eff129f5cd7b1c0b3eb216775adddd1264f1
hs.qian <2829929040@qq.com>
2023-12-25 17:59:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,29 @@ import { Stream } from '../scripts/models'
 export default {
 	data() {
 		return {
-			streams: <any>[]
+			streams: <Stream[]>[],
+			originalStreams: <Stream[]>[],
+			form: {
+				keywords: ""
+			},
+			options: {
+				channel: <string[]>[],
+				groupTitle: [],
+				isNSFW: <String[]>[],
+				languages: [],
+				name: [],
+				quality: <string[]>[]
+			},
+			checked: {
+				channel: undefined,
+				groupTitle: undefined,
+				isNSFW: "nsfw",
+				languages: undefined,
+				name: undefined,
+				quality: undefined
+			}
+			
+
 		}
 	},
 	mounted() {
@@ -15,16 +37,69 @@ export default {
 		async load(): Promise<void> {
 			let data = await get_streams()
 			this.streams = data
-			let d = data.filter(item => item.isNSFW)
-			console.log(d.at(0)?.url)
+			this.originalStreams = JSON.parse(JSON.stringify(data))
+			this.options.quality = Array.from(new Set(data.map(item => item.quality ?? "undefined")))
+			this.options.isNSFW = Array.from(new Set(data.map(item => (item.isNSFW ? "nsfw": "normal"))))
+			
+		},
+		filter(data:Stream[]) {
+
 		}
 	}
 }
 </script>
 
 <template>
-	<div>
-	</div>
+	<el-card class="container">
+		<div class="options">
+			<el-select v-model="checked.quality" class="m-2" placeholder="quality" size="small">
+				<el-option
+				v-for="item in options.quality"
+				:key="item"
+				:label="item"
+				:value="item"
+				/>
+			</el-select>
+			<el-select v-model="checked.isNSFW" class="m-2" placeholder="nsfw" size="small" style="margin-left: 10px;">
+				<el-option
+				v-for="item in options.isNSFW"
+				:key="item"
+				:label="item"
+				:value="item"
+				/>
+			</el-select>
+			<el-button type="success" size="small" style="margin-left: 10px;">Search</el-button>
+		</div>
+		<div class="stream-list">
+			<div class="stream-item" v-for="(item,idx) in streams" :key="idx">
+				<span class="stream-title">{{ item.name }}</span>
+				<a class="stream-url" :href="item.url">GO</a>
+			</div>
+		</div>
+		
+		
+	</el-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+	display: flex;
+}
+.options {
+	height: 50px;
+}
+.stream-list {
+	/* height: 80vh; */
+	overflow-y: auto;
+	margin-top: 20px;
+	flex: 1;
+}
+.stream-item {
+	display: flex;
+	flex-direction: row;
+	margin-bottom: 10px;
+}
+.stream-url {
+	margin-left: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElIcons from '@element-plus/icons'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+// for (const name in ElIcons){
+// 	app.component(name, ElIcons[name])
+// }
+
+app.use(router).use(ElementPlus).mount('#app')
